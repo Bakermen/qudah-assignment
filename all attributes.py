08e5dfe0c91a5cb530c14c81a3a6a7b1e3f9a695
Bakermen <54038795+Bakermen@users.noreply.github.com>
2021-11-24 18:22:51 +0200

    done

---
+++ all attributes.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
+dataframe = pd.read_csv("diabetes_dataset.csv")
+
+attributes = dataframe.loc[:, dataframe.columns !='Outcome']
+outcome = dataframe.loc[:, 'Outcome']
+
+train_att = attributes[:700]
+train_outcome = outcome[:700]
+test_att = attributes[700:]
+test_outcome = outcome[700:]
+
+model = DecisionTreeClassifier()
+model = model.fit(train_att, train_outcome)
+ 
+prediction = model.predict(test_att)
+
+con_matrix = confusion_matrix(prediction, test_outcome)
+print(accuracy_score(test_outcome, prediction), con_matrix)
